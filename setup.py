836545451874e50d81aff7fb4a6ff17a6cacd86a
Clark Mckenzie <clarkmckenzie@googlemail.com>
2014-02-14 19:47:50 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import sys, os
+
+setup(
+      name='pybrowserstack-screenshots',
+      version='0.1',
+      description="api wrapper and python client for Browserstack Screenshots, including phantomCSS support",
+      long_description=""" """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='browserstack screenshots phantomCSS',
+      author='Clark Mckenzie',
+      author_email='clarkmckenzie@googlemail.com',
+      url='http://github.com/cmck/pybrowserstack-screenshots',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            "simplejson>=3.3.2",
+            "requests>=2.2.0",
+            "Pillow>=2.3.0",
+      ],
+      )
